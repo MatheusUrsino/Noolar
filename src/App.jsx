2af7f1050a1a3a20d45a614efa78bec8d51e3ba2
Matheus Ursino <matheusursino008@gmail.com>
2024-12-08 01:05:16 -0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import "./App.css";
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppMenu from "./menu-noolar/MenuPrincipalNoolar/components/menu";
 import AppAreaDaLeitura from "./AreaDaLeitura/AppAreaDaLeitura";
 import AppDocs from "./Documentos/AppDocs";
 import AppDocsNecessarios from "./DocumentosNecessarios/AppDocsNecessarios";
 import AppDaComunidades from "./ComunidadesArea/AppComunidades.jsx";
 import NavbarApp from "./header/NavBarApp.jsx";
-import VLibras from "@djpfs/react-vlibras";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://vlibras.gov.br/app/vlibras-plugin.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      new window.VLibras.Widget("https://vlibras.gov.br/app");
+    };
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <>
       <Router>
-        <VLibras forceOnload={true} />
+        <div vw="true" className="vw-widget" />
         <header className="aaaaaaaaaaa">
           <NavbarApp />
         </header>
@@ -26,10 +35,7 @@ function App() {
           <Route path="/" element={<AppMenu />} />
           <Route path="/AreaDaLeitura" element={<AppAreaDaLeitura />} />
           <Route path="/Documentos" element={<AppDocs />} />
-          <Route
-            path="/DocumentosNecessarios"
-            element={<AppDocsNecessarios />}
-          />
+          <Route path="/DocumentosNecessarios" element={<AppDocsNecessarios />} />
           <Route path="/Comunidades" element={<AppDaComunidades />} />
         </Routes>
       </Router>
